90a8ae505c1b116ea6b1b3068eed0febe9fb2a3a
Gabriel Duarte <gabriel160499@gmail.com>
2018-03-15 16:23:57 -0300

    Ex4

---
+++ Ex4/src/Quadrado.java
@@ -0,0 +1,22 @@
+
+public class Quadrado {
+	
+	public int lado;
+	public int area;
+	public int perimetro;
+	public Quadrado(int lado) {
+		// TODO Auto-generated constructor stub
+		this.lado = lado;
+		calcularArea();
+		calcularPerimetro();
+	}
+	public void calcularArea() {
+		area = lado * lado;
+	}
+	public void calcularPerimetro() {
+		perimetro = lado *4;
+	}
+	public void println() {
+		System.out.println("Lado: " + this.lado + "m\nArea: "+ this.area + "m\nPerimetro: " + this.perimetro);
+	}
+}
